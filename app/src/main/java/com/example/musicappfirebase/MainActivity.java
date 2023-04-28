5d2512d816d4d2debd071c23cb8af60f4cc7f867
DeCeNt <https://github.com/SaniaKashaf/MY-First-Project.git>
2023-04-28 10:49:58 -0700

    First Part:37min Upload MP3 file on firebase

---
+++ app/src/main/java/com/example/musicappfirebase/MainActivity.java
@@ -0,0 +1,152 @@
+package com.example.musicappfirebase;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.OpenableColumns;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+@SuppressWarnings("ALL")
+public class MainActivity extends AppCompatActivity {
+    
+    private boolean checkPermission = false;
+    Uri uri;
+    String songName,songUrl;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.custom_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==R.id.nav_upload){
+
+                pickSong();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void pickSong() {
+
+        Intent intent_upload = new Intent();
+        intent_upload.setType("audio/*");
+        intent_upload.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent_upload,1);
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode ==1){
+            if(resultCode ==RESULT_OK){
+
+                uri = data.getData();
+
+                Cursor mcursor = getApplicationContext().getContentResolver()
+                        .query(uri,null,null,null,null);
+
+                int indexedname = mcursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                mcursor.moveToFirst();
+                songName = mcursor.getString(indexedname);
+                mcursor.close();
+
+                uploadSongToFirebaseStorage();
+
+
+            }
+        }
+
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void uploadSongToFirebaseStorage() {
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference()
+                .child("Songs").child(uri.getLastPathSegment());
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.show();
+
+        storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+
+                Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                while (!uriTask.isComplete());
+                Uri urlSong = uriTask.getResult();
+                songUrl = urlSong.toString();
+
+                uploadDetailsToDatabase();
+                progressDialog.dismiss();
+
+
+
+
+
+            }
+
+            private void uploadDetailsToDatabase() {
+                Song songObj = new Song(songName,songUrl);
+
+                FirebaseDatabase.getInstance().getReference("Songs")
+                        .push().setValue(songObj).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(MainActivity.this, "Song Uploaded", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                double progres = (100.0*taskSnapshot.getBytesTransferred())/taskSnapshot.getTotalByteCount();
+                int currentProgress = (int)progres;
+                progressDialog.setMessage("Uploaded: "+currentProgress+"%");
+            }
+        });
+    }
+
+}
